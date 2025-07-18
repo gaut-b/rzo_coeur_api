222446a2efbb5e93ed54ce81164c45097dfea813
Clement Viel <vielclement@gmail.com>
2025-07-18 09:36:35 +0200

    add permissions w/o enforcing yet

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import GroupAdmin
+
+from django.db import models
 
 from .models import Magasin
 from .models import Centre_social
@@ -12,3 +16,4 @@ admin.site.register(Centre_social)
 admin.site.register(Beneficiaire)
 admin.site.register(Client)
 admin.site.register(Utilisateur)
+admin.site.register(Panier)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Utilisateur(models.Model):
     id_utilisateur = models.BigIntegerField(primary_key=True)
     role = models.CharField(max_length=100)
@@ -23,25 +24,44 @@ class Centre_social(models.Model):
     address = models.CharField(max_length=200)
     mail = models.CharField(max_length=200)
 
+    class Meta:
+        permissions = [
+            ("can_create_magasin", "Can create a magasin object"),
+            ("can_create_beneficiaire", "Can create a beneficiaire object"),
+            ("can_create_panier", "Can create a panier object"),
+        ]
+
     def __str__(self):
         return self.name
+
 
 class Magasin(models.Model):
     id_magasin = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     centre_social = models.ForeignKey(Centre_social, on_delete=models.CASCADE)
+    # Methods proposal
+    # - create article list
+    # - notify list suspendus
+
+    class Meta:
+        permissions = [
+            ("can_create_client", "Can create a client object"),
+            ("can_create_panier", "Can create a panier object"),
+        ]
 
     def __str__(self):
         return self.name
 
 
 class Beneficiaire(models.Model):
-    #TODO: id must be generated 
+    # TODO: id must be generated
     id_beneficiaire = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     centre_social = models.ForeignKey(Centre_social, on_delete=models.CASCADE)
+    # Methods proposal
+    # -
 
     def __str__(self):
         return self.name
@@ -53,18 +73,27 @@ class Article_scanned(models.Model):
     qte_suspendus = models.IntegerField()
     client_origine = models.ForeignKey(Client, on_delete=models.CASCADE)
     magasin_origine = models.ForeignKey(Magasin, on_delete=models.CASCADE)
+    # Methods proposal
+    # -
 
     def __str__(self):
         return self.name
 
+
 class ArticleAssigne(models.Model):
     article = models.ForeignKey(Article_scanned, on_delete=models.CASCADE)
     centre_social = models.ForeignKey(Centre_social, on_delete=models.CASCADE)
+    # Methods proposal
+    # -
+
 
 class Panier(models.Model):
     id_panier = models.BigIntegerField(primary_key=True)
-    article_assigne = models.ForeignKey(ArticleAssigne, on_delete=models.CASCADE)
+    article_assigne = models.ForeignKey(
+        ArticleAssigne, on_delete=models.CASCADE)
     magasin = models.ForeignKey(Magasin, on_delete=models.CASCADE)
 
+    # Methods proposal
+    # -
     def __str__(self):
         return self.id_panier
