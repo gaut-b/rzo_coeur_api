277c293ee0816c8270833089d9cb5c085bd3271d
Clement Viel <vielclement@gmail.com>
2026-02-16 21:50:50 +0100

    cart attribution: create a page where a social worker can create a cart, assigned articles to it and assign it to a recipient

--- config/urls.py
+++ config/urls.py
@@ -22,11 +22,13 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from api.admin import shop_admin_site, social_admin_site
+from api.cart_attributions import cart_attrib_admin_site
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("shop-admin/", shop_admin_site.urls),
     path("social-admin/", social_admin_site.urls),
+    path("cart-attr/", cart_attrib_admin_site.urls),
     path("api/auth/", include("auth_kit.urls")),
     path("api/", include("api.urls")),
 ]
